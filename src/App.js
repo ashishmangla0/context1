24c2f65db484c7ee1aa61343302c52c56321ea0b
Ashish Mangla <ashmangl@publicisgroupe.net>
2022-06-28 17:01:01 +0530

    add new code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useContext } from "react";
 import { ThemeContext } from "./context/ThemeProvider";
+import { BookContext } from "./context/BookContext";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 
 function App() {
   const { themeDark, handleThemeDark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
 
   return (
     <div className="App">
       <button onClick={handleThemeDark}>Swit theme</button>
       {themeDark ? "aaaaaa" : "bbbbbb"}
+      <br />
+      book length {books.length}
+      <BookList />
+      <BookForm/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './context/ThemeProvider';
+import { BookContextProvider } from './context/BookContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
+      <BookContextProvider>
     <App />
+    </BookContextProvider>
     </ThemeProvider>  
   </React.StrictMode>
 );
